78b83e65e6a1367f567c29db4506802c2a7b488d
taka <todoryu@gol.com>
2021-04-06 19:13:50 +0900

    Q6をQ7と勘違いしていたので再度解答しました

--- task.rb
+++ task.rb
@@ -49,15 +49,14 @@ def q6
   numbers1 = [1, 2, 3, 4, 5]
 
   # 以下に回答を記載
-  numbers2 = []
-  numbers2 = numbers1.map(&:to_s)
-  p numbers2
+  p numbers1.map{|x| x * 10}
 end
 
 def q7
   array = ["1", "2", "3", "4", "5"]
 
   # 以下に回答を記載
+
   # 以下は変更しないで下さい
   p array
 end
